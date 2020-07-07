5fd56573b835752a1c61d06be5a98db910265afa
Francis <bilodeaurobillardfrancis@gmail.com>
2020-07-07 15:40:06 -0400

    Chore: webpack configurations

---
+++ webpack.dev.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+        filename: 'Bundle.js'
+    },
+    devServer: {
+        port: 8001,
+        contentBase: path.join(__dirname, './dist')
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    module: {
+        rules: [
+            {
+                test: /\js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.sass$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./'index.html"
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write logs to console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
+};
