d835de594f0f7aeff9f87936f468cd1e7ad27a8f
Francis <bilodeaurobillardfrancis@gmail.com>
2020-07-10 15:26:21 -0400

    Feat: added all needed endpoints All api fetches are done server side to easily hide API keys.

--- src/server/index.js
+++ src/server/index.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser');
 const cors  = require('cors');
 const express = require('express');
 const dotenv = require('dotenv');
+const fetch = require("node-fetch");
+const { raw } = require('body-parser');
 const app = express();
 
-const WEATHERBIT_KEY = process.env.WEATHER_BIT_KEY
-const GEONAMES_USERNAME = process.env.GEONAMES_USERNAME
-
 dotenv.config();
+
+const WEATHERBIT_KEY = process.env.WEATHERBIT_KEY
+const MAPBOX_KEY = process.env.MAPBOX_KEY
+const PIXABAY_KEY = process.env.PIXABAY_KEY
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -21,12 +24,44 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html');
 });
 
-app.post('/location', localize(req, res));
+app.post('/location', async(req, res)=>{
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${req.body.cityName}.json?country=ca&access_token=${MAPBOX_KEY}`
+    res.send(await fetchData(url))
+});
 
+app.post('/weekForecast', async(req, res)=>{
+    const lat = req.body.latitude;
+    const lon = req.body.longitude;
+    const day = req.body.day;
 
-function localize(req, res){
-    let url = `http://api.geonames.org/searchJSON?q=${req.body.cityName}&maxRows=1&username=${GEONAMES_USERNAME}`
-    res.send(fetch(url))
+    const url =`https://api.weatherbit.io/v2.0/forecast/daily?lat=76.5668&lon=-78.1018&key=${WEATHERBIT_KEY}`
+    res.send(await fetchData(url))
+});
+
+app.post('/normalForecast', async(req, res)=> {
+    const lat = req.body.latitude;
+    const lon = req.body.longitude;
+    const date = req.body.date
+
+    const url = `https://api.weatherbit.io/v2.0/normals?lat=38.0&lon=-78.0&start_day=02-02&end_day=03-01&tp=daily&key=${WEATHERBIT_KEY}`
+
+    res.send(await fetchData(url));
+});
+
+app.post('/picture', async(req, res)=>{
+    const url =`https://pixabay.com/api/?key=${PIXABAY_KEY}&q=${req.body.city}&image_type=photo&pretty=true`
+    res.send(await fetchData(url))
+});
+
+async function fetchData(url){
+    const response = await fetch(url);
+
+    try{
+        const data = await response.json()
+        return data;
+    }catch{
+        console.log("error", error);
+    }
 }
 
 // Designates what port the app will listen to for incoming requests
