4079212449b2f32a2ca38e2e5b05b77b7c90f812
Francis <bilodeaurobillardfrancis@gmail.com>
2020-07-16 14:57:47 -0400

    Test: formhandler tests

---
+++ test/formHandler.test.js
@@ -0,0 +1,26 @@
+const { diffBetweenDays, coordinates, weekForecast } = require('../src/client/js/formHandler')
+
+test('Correct calculation of difference between two dates', () =>{
+    const date1 = new Date(2020, 5, 8);
+    const date2 = new Date(2020, 5, 5);
+    expect(diffBetweenDays(date1, date2)).toBe(3);
+});
+
+test('Error: missing one date', () => {
+    const date1 = new Date(2020, 5, 5);
+    expect(() => diffBetweenDays(date1)).toThrow();
+});
+
+test('Correct coordinates', async ()=>{
+    const expectedRes = {
+        'latitude':-78.1018,
+        'longitude': 48.5668
+    }
+
+    const data = await coordinates('Amos', 'Canada');
+    expect(data).toEqual(expectedRes);
+});
+
+test('Error: country does not exist', async ()=>{
+    expect(async() => await coordinates('Amos', 'Kalimdor').toThrow());
+});
