9299eccaf3d08cb98245ac0a3f4d79624f3c55b4
Francis <bilodeaurobillardfrancis@gmail.com>
2020-07-16 15:05:39 -0400

    Feat: functions to fetch trip informations

--- src/client/js/formhandler.js
+++ src/client/js/formhandler.js
@@ -1,67 +1,103 @@
-import { validText, validDate } from "./validateInput"
+import { validText } from "./validateInput";
+import regeneratorRuntime from "regenerator-runtime";
+const fetch = require("node-fetch");
+
 
 function createTrip(event){
-    event.preventDefault()
+    event.preventDefault();
 
-    
-    coordinates()
+    const city = document.getElementById('inputLocation').value;
+    const country = document.getElementById('inputCountry').value;
+    const tripDate = new Date(document.getElementById('inputDate').value);
+    const todayDate = new Date();
+    const diff = diffBetweenDays(tripDate, todayDate);
+
+    if (diff < 0) {
+        throw new Error('Trip date must in the future')
+    }
+
+    coordinates(city, country)
     .then((res) => {
         console.log(res)
+
+
+        if (diff > 7) {
+
+        } else {
+            //console.log(res);
+            weekForecast('http://localhost:8010/weekForecast', res );
+        }
     });
-
-    forecast()
-
-}
+};
 
 
 const location = async(url='', data={}) => {
-    const res = await fetchURL(url, data)
-    try{
+
+    const res = await fetch(url, getOptions(data));
+
+    try {
         const locations = await res.json();
         const coordinates = locations.features[0].geometry.coordinates
         const response = {
-            "latitude": coordinates[0],
-            "longitude": coordinates[1]
+            "latitude": coordinates[1],
+            "longitude": coordinates[0]
         }
         return response;
-    }catch(error){
+    } catch(error) {
         console.log("error", error );
     }
 };
 
-async function fetchURL(url, data){
-    // TODO: To put in a variable, not a function.
-    return await fetch(url, {
+const weekForecast = async(url='', data={}) => {
+
+    const res = await fetch(url, getOptions(data));
+
+    try {
+        const forecast = await res.json();
+        console.log(forecast)
+
+    } catch(error) {
+        console.log("error", error );
+    }
+}
+
+const coordinates = async(city, country) => {
+    try{
+        const countryCodeRes = await(fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`));
+
+        if (countryCodeRes.status == 404) {
+            throw new Error('Country does not exist')
+        }
+
+        const countryCode = await countryCodeRes.json();
+        return await location('http://localhost:8010/location', {cityName: city, country: countryCode[0].alpha2Code});
+
+    } catch(error) {
+        console.log("error", error)
+    }
+}
+
+function diffBetweenDays(date1, date2) {
+
+    if ( date1 === undefined || date2 === undefined) {
+        throw new Error('Both dates must be provided');
+    }
+
+    const diffInTime = date1.getTime() - date2.getTime();
+    const diffInDays = diffInTime / (1000 * 3600 * 24);
+
+    return diffInDays;
+}
+
+function getOptions(data) {
+    return {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-    });
+    }
 }
 
-const coordinates = async() => {
-    // TODO: TEST!
-    const city = document.getElementById('inputLocation').value;
-    const country = document.getElementById('inputCountry').value;
-
-    const res = await( fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`));
-    const countryCode = await res.json();
-    const coordinates = await location('http://localhost:8010/location', {cityName: city, country: countryCode[0].alpha2Code});
-
-    return coordinates
-}
-
-const forecast = async(coordinates) => {
-    // TODO: parse DATE. TEST!
-    const tripDate = new Date(document.getElementById('inputDate').value);
-    const todayDate = new Date();
-
-    const diffInTime = tripDate.getTime() - todayDate.getTime();
-    const diffInDays = diffInTime / (1000 * 3600 * 24);
-    console.log(diffInDays);
-
-}
-
-export { createTrip }
+export { createTrip, diffBetweenDays, coordinates, weekForecast }
